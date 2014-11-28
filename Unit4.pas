900953c4425beda6d63651b4903cff2285dd8e03
will <guillermo.embarcadero.com>
2014-11-28 17:27:31 +0100

    Mi commit

--- Unit4.pas
+++ Unit4.pas
@@ -8,6 +8,7 @@ uses
 
 type
   TForm4 = class(TForm)
+    procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
   public
@@ -20,5 +21,10 @@ var
 implementation
 
 {$R *.fmx}
+
+procedure TForm4.FormCreate(Sender: TObject);
+begin
+showmessage('');
+end;
 
 end.
