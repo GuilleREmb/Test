9990a78e77e44c09e1978100baf53505faf0016d
Will <guillermo.rodenas@embarcadero.com>
2014-12-01 13:51:02 +0100

    sadsadas

--- Unit9.pas
+++ Unit9.pas
@@ -4,10 +4,11 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls;
 
 type
   TForm9 = class(TForm)
+    Button1: TButton;
   private
     { Private declarations }
   public
