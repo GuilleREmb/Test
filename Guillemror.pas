846cdffc75da645dea23d43d4a1f856906d9d9d2
Will <guillermo.rodenas@embarcadero.com>
2014-12-01 14:08:24 +0100

    fgfgf

---
+++ Guillemror.pas
@@ -0,0 +1,37 @@
+unit Guillemror;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls;
+
+type
+  TForm4 = class(TForm)
+    Button1: TButton;
+    procedure FormCreate(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form4: TForm4;
+
+implementation
+
+{$R *.fmx}
+
+procedure TForm4.Button1Click(Sender: TObject);
+begin
+showmessage('');
+end;
+
+procedure TForm4.FormCreate(Sender: TObject);
+begin
+showmessage('');
+end;
+
+end.
